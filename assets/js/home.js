475d4d996e2c0ad1bae6af2547a70f8fc719c9ce
wazizstore <chatom2007@gmail.com>
2026-07-10 16:36:54 +0200

    Delete assets directory

--- assets/js/home.js
+++
@@ -1,9 +0,0 @@
-import { products } from './data.js';
-import { renderGrid } from './product-grid.js';
-import { initCartDrawer, renderCart } from './ui.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initCartDrawer();
-  renderCart();
-  renderGrid('featuredGrid', products);
-});
